4487334f5b1ad9be22c7fd1fe7ee26285364a1e2
Peter Josh <phanhuuphuoc.vn@gmail.com>
2023-10-03 10:23:21 +0700

    update layout

---
+++ the_final/src/components/templates/Home/HomeTemplates.tsx
@@ -0,0 +1,7 @@
+export const HomeTemplates = () => {
+  return (
+    <div>HomeTemplates</div>
+  )
+}
+
+export default HomeTemplates
---
+++ the_final/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {HomeTemplates} from 'component'
+
+const HomePage = () => {
+  return (
+    <HomeTemplates/>
+  )
+}
+
+export default HomePage
