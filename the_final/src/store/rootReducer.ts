3386feae63a8cde18828bf7aedddb735546b9c8c
Peter Josh <phanhuuphuoc.vn@gmail.com>
2023-10-11 20:50:06 +0700

    merge movielist

--- the_final/src/store/rootReducer.ts
+++ the_final/src/store/rootReducer.ts
@@ -1,11 +1,10 @@
 import {combineReducers} from '@reduxjs/toolkit'
 import { manageUserReducer } from './manageUser/slice'
+import { manageCourseReducer } from "./CourseManagement/slice";
 export const rootReducer = combineReducers({
     manageUser:manageUserReducer,
-
-})import { combineReducers } from "@reduxjs/toolkit";
-import { manageCourseReducer } from "./CourseManagement/slice";
-
-export const rootReducer = combineReducers({
     manageCourse: manageCourseReducer,
 })
+
+
+
