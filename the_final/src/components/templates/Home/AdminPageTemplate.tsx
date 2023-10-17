dbe8cec9f4c95f35cb92e4fc8089c625a654e469
Peter Josh <phanhuuphuoc.vn@gmail.com>
2023-10-17 15:39:16 +0700

    add admin

--- the_final/src/components/templates/Home/AdminPageTemplate.tsx
+++ the_final/src/components/templates/Home/AdminPageTemplate.tsx
@@ -1,7 +1,20 @@
-const AdminPageTemplate = () => {
+import { AdminNavbar } from "components/templates/Admin";
+import { Outlet } from "react-router-dom";
+
+export const AdminPageTemplate = () => {
   return (
-    <div></div>
-  )
+    <div
+      className="grid grid-cols-5 h-[100vh] overflow-hidden gap-0"
+      style={{ backgroundColor: "cyan" }}
+    >
+      <div className="col-span-1">
+        <AdminNavbar />
+      </div>
+      <div className=" col-span-4 flex items-center">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
 export default AdminPageTemplate
--- the_final/src/router/index.tsx
+++ the_final/src/router/index.tsx
@@ -1,50 +1,44 @@
-import { HomeTemplates } from 'components'
-import HomeLayout from 'components/layouts/HomeLayout'
-import { PATH } from 'constant'
-import { AllCourse, CourseDetailPage, CourseFollowMenu } from 'pages'
-import AdminPage from 'pages/AdminPage'
-import UserInfomation from 'pages/UserInformation/UserInfomation'
+import { HomeTemplates } from "components";
+import HomeLayout from "components/layouts/HomeLayout";
+import { PATH } from "constant";
+import { AllCourse, CourseDetailPage, CourseFollowMenu } from "pages";
+import AdminPage from "pages/AdminPage";
+import UserInfomation from "pages/UserInformation/UserInfomation";
 
 // import UserInfomation from 'pages/UserInformation/UserInfomation'
 
-import {RouteObject} from 'react-router-dom'
+import { RouteObject } from "react-router-dom";
 
+export const router: RouteObject[] = [
+  {
+    element: <HomeLayout />,
+    path: "/",
+    children: [
+      {
+        element: <HomeTemplates />,
+        index: true,
+      },
+      {
+        element: <UserInfomation />,
+        path: PATH.user,
+      },
 
-export const router:RouteObject[] = [
-    {
-        element:<HomeLayout/>,
-        path:'/',
-        children:[
-            {
-                element:<HomeTemplates/>,
-                index:true,
-            },
-            {
-                element: <UserInfomation/>,
-                path: PATH.user,
-            },
-
-            {
-                element:<CourseDetailPage/>,
-                path: PATH.detail,
-            },
-            {
-                element:<CourseFollowMenu/>,
-                path: PATH.course,
-            },
-            {
-                element:<AllCourse/>,
-                path:PATH.allcourse,
-            },
-            {
-                element:<AdminPage/>,
-                path:PATH.admin,
-            }
-
-            
-
-        ]
-    }
-    
-  
-]
+      {
+        element: <CourseDetailPage />,
+        path: PATH.detail,
+      },
+      {
+        element: <CourseFollowMenu />,
+        path: PATH.course,
+      },
+      {
+        element: <AllCourse />,
+        path: PATH.allcourse,
+      },
+    ],
+  },
+  {
+    element: <AdminPage />,
+    path: PATH.admin,
+  },
+];
